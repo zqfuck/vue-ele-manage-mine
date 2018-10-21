0bcdd7565619e7d0cc7540209b775359faa651cf
zq <1083518075@qq.com>
2018-10-21 19:53:01 +0800

    echats  data

---
+++ vue-manage-ele/src/js/api.js
@@ -0,0 +1,73 @@
+/**
+ * Created by zq on 2018/10/21.
+ */
+import axios from 'axios'
+// axios 配置
+axios.defaults.timeout = 3000
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+axios.defaults.baseURL = 'http://elm.cangdu.org'
+
+export function fetchPost (url, params) {
+  return new Promise((resolve, reject) => {
+    axios.post(url, params)
+      .then(response => {
+        resolve(response.data)
+      }, err => {
+        reject(err)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+export function fetchGet (url, params) {
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
+      params: params
+    }).then(res => {
+      resolve(res.data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
+export default {
+  /**
+   * 用户注册量
+   */
+  userCount (params) {
+    return fetchGet('/statis/user/' + params + '/count')
+  },
+  /**
+   * 某一天订单数量
+   */
+  orderCount (params) {
+    return fetchGet('/statis/order/' + params + '/count')
+  },
+  /**
+   * 某一天管理员注册量
+   */
+  adminDayCount (params) {
+    return fetchGet('/statis/admin/' + params + '/count')
+  },
+  /**
+   * 获取用户数量
+   */
+  getUserCount (params) {
+    return fetchGet('/v1/users/count', params)
+  },
+  /**
+   * 获取订单数量
+   */
+  getOrderCount (params) {
+    return fetchGet('/bos/orders/count', params)
+  },
+  /**
+   * 管理员数量
+   */
+  adminCount (params) {
+    return fetchGet('/admin/count')
+  }
+}
--- vue-manage-ele/src/pages/Home.vue
+++ vue-manage-ele/src/pages/Home.vue
@@ -5,34 +5,88 @@
       <h2 class="home_title">数据统计</h2>
       <el-row :gutter="20" style="margin-bottom: 20px;">
         <el-col :span="4"><div class="data_list"><span class="data_head">当日数据 :</span></div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>新增用户</div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>新增订单</div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>新增管理员</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{userCount}}</span>新增用户</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{orderCount}}</span>新增订单</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{adminCount}}</span>新增管理员</div></el-col>
       </el-row>
       <el-row :gutter="20" style="margin-bottom: 20px;">
         <el-col :span="4"><div class="data_list"><span class="data_head">总数据 :</span></div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>注册用户</div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>订单</div></el-col>
-        <el-col :span="4"><div class="data_list"><span class="data_num"></span>管理员</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{allUserCount}}</span>注册用户</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{allOrderCount}}</span>订单</div></el-col>
+        <el-col :span="4"><div class="data_list"><span class="data_num">{{allAdminCount}}</span>管理员</div></el-col>
       </el-row>
     </section>
+    <tendency :sevenDate='sevenDate' :sevenDay='sevenDay'></tendency>
   </div>
 </template>
 
 <script>
 import HeadTop from '@/components/HeadTop'
+import Tendency from '@/components/Tendency'
+import dtime from 'moment'
+import api from '@/js/api.js'
 export default{
   name: 'Home',
   data () {
     return {
-
+      userCount: null,
+      orderCount: null,
+      adminCount: null,
+      allUserCount: null,
+      allOrderCount: null,
+      allAdminCount: null,
+      sevenDay: [],
+      sevenDate: [[],[],[]],
     }
   },
   components: {
-    HeadTop
+    HeadTop,
+    Tendency
+  },
+  mounted () {
+    this.initDada()
+    for (let i = 6; i > -1; i--) {
+      const date = dtime(new Date().getTime() - 86400000*i).format('YYYY-MM-DD')
+      this.sevenDay.push(date)
+    }
+    this.getSevenData()
   },
   methods: {
-
+    initDada () {
+      var rawDate = dtime().format('YYYY-MM-DD')
+      console.log(rawDate)
+      Promise.all([api.userCount(rawDate), api.orderCount(rawDate), api.adminDayCount(rawDate), api.getUserCount(), api.getOrderCount(), api.adminCount()]).then(res => {
+        console.log(res)
+        this.userCount = res[0].count
+        this.orderCount = res[1].count
+        this.adminCount = res[2].count
+        this.allUserCount = res[3].count
+        this.allOrderCount = res[4].count
+        this.allAdminCount = res[5].count
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    getSevenData(){
+      const apiArr = [[],[],[]]
+      this.sevenDay.forEach(item => {
+        apiArr[0].push(api.userCount(item))
+        apiArr[1].push(api.orderCount(item))
+        apiArr[2].push(api.adminDayCount(item))
+      })
+      const promiseArr = [...apiArr[0], ...apiArr[1], ...apiArr[2]]
+      Promise.all(promiseArr).then(res => {
+        const resArr = [[],[],[]]
+        res.forEach((item, index) => {
+          if (item.status == 1) {
+            resArr[Math.floor(index/7)].push(item.count)
+          }
+        })
+        this.sevenDate = resArr;
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
@@ -67,5 +121,10 @@ section{
   height: 100%;
   color: #fff;
   background: #FF9800;
+}
+.data_num{
+  font-size: 26px;
+  color: mediumseagreen;
+  margin-right: 5px;
 }
 </style>
